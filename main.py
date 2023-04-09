4d0adc080afb90f509f5f2455047d4a4e6a17f94
crimera <stevendavemiranda2@gmail.com>
2023-04-09 21:00:43 +0800

    Now makes a folder named with the code for sorting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yabe
 import asmrone
 import shutil
 import utils
+import os
 
 transcriber = VideoTranscriber("whisper-large-v2-ct2/")
 drivepath = "/content/drive/MyDrive/VoiceWorks"
@@ -11,6 +12,10 @@ directory = get_dir(url)
 code = get_code(url)
 tree = get_work(code)
 links = get_track_urls(directory, tree)
+
+output_path = f"{drivepath}/RJ{code}/"
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
 
 print(f"found: {len(links)} tracks")
 
@@ -27,12 +32,17 @@ for index, link in enumerate(links):
 
     # download
     filename = download(track_link)
+    output_filename = f"{filename}.mp4"
+    output_subs_filename = f"{filename}.srt"
 
     # transcribe
     print("transcribing...")
-    transcriber.transcribe_and_embed(filename)
-    try:
-        shutil.move(f"{filename}.mp4", drivepath)
-        shutil.move(f"{filename}.srt", drivepath)
-    except Exception:
-        print("File already exists")
+    if not os.path.exists(output_filename):
+      print("file already exists")
+      transcriber.transcribe_and_embed(filename)
+
+    if not os.path.exists(f"{output_path}/{output_filename}"):
+        shutil.move(output_filename, output_path)
+        shutil.move(output_subs_filename, output_path)
+    else:
+        print("already exists on drive")
