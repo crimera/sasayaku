8dff86de94e7a187ad417a0be0dbd3404675b0a6
crimera <stevendavemiranda2@gmail.com>
2023-04-20 00:00:20 +0800

    do not download if file already exist

--- yabetest.py
+++ yabetest.py
@@ -1,3 +1,3 @@
 from yabe import transcribe_and_embed, FasterWhisper, Whisper
 
-transcribe_and_embed(Whisper("base.en"), "sample/guraclip.opus")
+transcribe_and_embed(FasterWhisper("../../whisper-large-v2-ct2"), "sample/guraclip.opus")
