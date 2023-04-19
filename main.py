f18be018e1b69583c02e6ae0c3ab570e81bb991b
crimera <stevendavemiranda2@gmail.com>
2023-04-19 20:40:25 +0800

    remove url var

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import shutil
 import utils
 import os
 
-url = "https://asmr.one/work/RJ01026607?path=%5B%22%E6%9C%AC%E7%B7%A8%22,%22mp3%22,%22%E6%81%8B%E3%82%92%E3%81%97%E3%81%9F%E3%81%82%E3%81%AA%E3%81%9F%E3%81%B8%E3%80%82%22,%22%E3%81%AA%E3%82%86%E3%81%AE%E6%B0%97%E6%8C%81%E3%81%A1%E3%82%92%E5%8F%97%E3%81%91%E5%85%A5%E3%82%8C%E3%82%8B%E3%80%82%22,%22%E3%82%B7%E3%83%81%E3%83%A5%E3%82%A8%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%9C%E3%82%A4%E3%82%B9%22%5D#work-tree"
-
 def move(src: dict, out: str, makedir: bool):
     if not os.path.exists(out):
         if not mkdir:
